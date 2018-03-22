e02c198f69a6aaa46759f09bd0771af49e79ff76
Oliver Schneider <oliver@assarbad.net>
2018-03-22 21:29:41 +0000

    Adding the new function names previously added to ntnative.h

--- ntdll-stubs/ntdll-delayed-stubs.c
+++ ntdll-stubs/ntdll-delayed-stubs.c
@@ -667,3 +667,62 @@ RtlGetFullPathName_U(
     _Out_writes_bytes_(BufferLength) PWSTR Buffer,
     _Out_opt_ PWSTR *FilePart
     ) { return 0; }
+
+NTSTATUS
+NTAPI
+RtlUpcaseUnicodeString(
+    _Inout_ PUNICODE_STRING  DestinationString,
+    _In_ PCUNICODE_STRING SourceString,
+    _In_ BOOLEAN AllocateDestinationString
+    ) { return 0; }
+
+WCHAR
+NTAPI
+RtlDowncaseUnicodeChar(
+    _In_ WCHAR SourceCharacter
+    ) { return 0; }
+
+WCHAR
+NTAPI
+RtlUpcaseUnicodeChar(
+    _In_ WCHAR SourceCharacter
+    ) { return 0; }
+
+_Must_inspect_result_
+LONG
+NTAPI
+RtlCompareUnicodeString(
+    _In_ PCUNICODE_STRING String1,
+    _In_ PCUNICODE_STRING String2,
+    _In_ BOOLEAN CaseInSensitive
+    ) { return 0; }
+
+_Must_inspect_result_
+BOOLEAN
+NTAPI
+RtlEqualUnicodeString(
+    _In_ PCUNICODE_STRING String1,
+    _In_ PCUNICODE_STRING String2,
+    _In_ BOOLEAN CaseInSensitive
+    ) { return 0; }
+
+VOID
+NTAPI
+RtlCopyUnicodeString(
+    _In_ PUNICODE_STRING DestinationString,
+    _In_ PCUNICODE_STRING SourceString
+    ) { }
+
+NTSTATUS
+NTAPI
+RtlAppendUnicodeStringToString(
+    _In_ PUNICODE_STRING Destination,
+    _In_ PCUNICODE_STRING Source
+    ) { return 0; }
+
+NTSTATUS
+NTAPI
+RtlAppendUnicodeToString(
+    _In_ PUNICODE_STRING Destination,
+    _In_opt_ PCWSTR Source
+    ) { return 0; }
